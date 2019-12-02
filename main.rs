da724983f89508c7e14a63c90613c58eef2f74b1
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2019-12-02 17:00:11 +1100

    Rename input file in template

--- main.rs
+++ main.rs
@@ -4,6 +4,6 @@ use std::io::BufReader;
 use std::collections::HashSet;
 
 fn main() {
-    let f = File::open("input/1.txt").unwrap();
+    let f = File::open("input.txt").unwrap();
     let reader = BufReader::new(f);
 }
