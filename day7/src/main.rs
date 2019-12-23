81914504da46c00c69898de48418a4378ae5eb99
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2019-12-23 16:19:41 +1000

    Day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
         .split(',')
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
-    let mut phases = vec![0,1,2,3,4];
+    let mut phases = vec![5,6,7,8,9];
     let output = phases.permutation().fold(0, |acc, permuted_phases| {
         let current_output = run_amplifier(xs.clone(), permuted_phases);
         max(acc, current_output)
@@ -25,29 +25,39 @@ fn main() {
 }
 
 fn run_amplifier(instructions: Vec<i32>, phases: Vec<i32>) -> i32 {
-    phases.iter().fold(0, |acc, phase| {
-        // Pass the previous output to the next phase
-        run_program(instructions.clone(), *phase, acc)
-    })
+    let mut states = vec![State { 
+        pc: 0, 
+        memory: instructions.clone(), 
+        output: 0, 
+        terminated: false, 
+        changed_output: false, 
+        has_used_phase: false 
+    }; phases.len()];
+    while !(states.last().unwrap().terminated) {
+        states = phases.iter().zip(states.iter()).scan(states.last().unwrap().output, |output, (phase, state)| {
+            // Pass the previous output to the next phase
+            let result = run_program(*phase, state.clone(), *output);
+            *output = result.output;
+            Some(result)
+        }).collect();
+    }
+    states.last().unwrap().output
 }
 
-fn run_program(instructions: Vec<i32>, phase: i32, input: i32) -> i32 {
-    let mut state = State { pc: 0, memory: instructions.clone(), output: 0, terminated: false };
-    let mut gotten_input = false;
+fn run_program(phase: i32, mut state: State, prev_state_output: i32) -> State {
     loop {
         // println!("{:?}", xs);
         let (mode1, mode2, mode3, op) = parse_operation(state.memory[state.pc]);
-        state = execute_operation(mode1, mode2, mode3, op, state.clone(), || {
+        state = execute_operation(mode1, mode2, mode3, op, state.clone(), |has_used_phase: bool| {
             // If generators were stable, I'd do that
-            if gotten_input {
-                input
+            if has_used_phase {
+                prev_state_output
             } else {
-                gotten_input = true;
                 phase
             }
         });
-        if state.terminated {
-            return state.output
+        if state.terminated || state.changed_output {
+            return state
         }
     }
 }
@@ -58,57 +68,59 @@ struct State {
     memory: Vec<i32>,
     output: i32,
     terminated: bool,
+    changed_output: bool,
+    has_used_phase: bool,
 }
 
-fn execute_operation(mode1: u32, mode2: u32, mode3: u32, op: i32, state: State, mut get_input: impl FnMut() -> i32) -> State {
+fn execute_operation(mode1: u32, mode2: u32, mode3: u32, op: i32, state: State, mut get_input: impl FnMut(bool) -> i32) -> State {
     let pc = state.pc;
     let mut memory = state.memory;
     let output = state.output;
+    let has_used_phase = state.has_used_phase;
     let param1 = if op != 99 { get_param(mode1, memory.clone(), pc, 1) } else { 0 };
     return match op {
-        99 => State { pc, memory, output, terminated: true },
+        99 => State { pc, memory, output, terminated: true, changed_output: false, has_used_phase },
         1 => {
             // Add
             let param2 = get_param(mode2, memory.clone(), pc, 2);
             let target = memory[pc + 3];
             memory[target as usize] = param1 + param2;
-            State { pc: pc + 4, memory, output, terminated: false }
+            State { pc: pc + 4, memory, output, terminated: false, changed_output: false, has_used_phase }
         },
         2 => {
             // Multiply
             let param2 = get_param(mode2, memory.clone(), pc, 2);
             let target = memory[pc + 3];
             memory[target as usize] = param1 * param2;
-            State { pc: pc + 4, memory, output, terminated: false }
+            State { pc: pc + 4, memory, output, terminated: false, changed_output: false, has_used_phase }
         },
         3 => {
             // Input
-            let input = get_input();
+            let input = get_input(has_used_phase);
             let target = memory[pc + 1];
             memory[target as usize] = input;
-            State { pc: pc + 2, memory, output, terminated: false }
+            State { pc: pc + 2, memory, output, terminated: false, changed_output: false, has_used_phase: true }
         },
         4 => {
             // Output
-            println!("Output: {}, PC: {}", param1, pc);
-            State { pc: pc + 2, memory, output: param1, terminated: false }
+            State { pc: pc + 2, memory, output: param1, terminated: false, changed_output: true, has_used_phase }
         },
         5 => {
             // Jump if true
             let param2 = get_param(mode2, memory.clone(), pc, 2);
             if param1 != 0 {
-                State { pc: param2 as usize, memory, output, terminated: false }
+                State { pc: param2 as usize, memory, output, terminated: false, changed_output: false, has_used_phase }
             } else {
-                State { pc: pc + 3, memory, output, terminated: false }
+                State { pc: pc + 3, memory, output, terminated: false, changed_output: false, has_used_phase }
             }
         },
         6 => {
             // Jump if false
             let param2 = get_param(mode2, memory.clone(), pc, 2);
             if param1 == 0 {
-                State { pc: param2 as usize, memory, output, terminated: false }
+                State { pc: param2 as usize, memory, output, terminated: false, changed_output: false, has_used_phase }
             } else {
-                State { pc: pc + 3, memory, output, terminated: false }
+                State { pc: pc + 3, memory, output, terminated: false, changed_output: false, has_used_phase }
             }
         },
         7 => {
@@ -120,7 +132,7 @@ fn execute_operation(mode1: u32, mode2: u32, mode3: u32, op: i32, state: State, 
             } else {
                 memory[target] = 0;
             }
-            State { pc: pc + 4, memory, output, terminated: false }
+            State { pc: pc + 4, memory, output, terminated: false, changed_output: false, has_used_phase }
         },
         8 => {
             // Equals
@@ -131,7 +143,7 @@ fn execute_operation(mode1: u32, mode2: u32, mode3: u32, op: i32, state: State, 
             } else {
                 memory[target] = 0;
             }
-            State { pc: pc + 4, memory, output, terminated: false }
+            State { pc: pc + 4, memory, output, terminated: false, changed_output: false, has_used_phase }
         },
         _ => {
             panic!("Unknown op {}", op);
