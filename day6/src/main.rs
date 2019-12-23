da2d54511047ed90bd7951dda4fac52b17247f94
Aidan Goldthorpe <agoldthorpe@atlassian.com>
2019-12-23 13:52:35 +1000

    Day 6 part 2 :tada

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,8 +2,9 @@ use std::io::BufRead;
 use std::fs::File;
 use std::io::BufReader;
 use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::result::Result;
 extern crate trees;
-use trees::tr;
 
 fn main() {
     let f = File::open("input.txt").unwrap();
@@ -14,8 +15,9 @@ fn main() {
         .map(|line| line.unwrap())
         .fold(HashMap::<String, Vec<String>>::new(), add_node);
     // DFS to count. Start from COM
-    let result = count_orbits(map, "COM".to_string(), 0);
-    println!("{}", result)
+    // let result = count_orbits(map, "COM".to_string(), 0);
+    let result = orbital_distance(map);
+    println!("{:?}", result)
 }
 
 fn add_node(mut map: HashMap::<String, Vec<String>>, unwrapped: String) -> HashMap::<String, Vec<String>> {
@@ -27,6 +29,44 @@ fn add_node(mut map: HashMap::<String, Vec<String>>, unwrapped: String) -> HashM
         .and_modify(|v| v.push(child.clone()))
         .or_insert(vec![child.clone()]);
     map
+}
+
+fn orbital_distance(map: HashMap::<String, Vec<String>>) -> Result<usize, ()> {
+    let mut you = find_path(map.clone(), "COM".to_string(), "YOU".to_string())?;
+    let mut santa = find_path(map.clone(), "COM".to_string(), "SAN".to_string())?;
+    // Strip common prefix
+    while you[0] == santa[0] {
+        you.pop_front();
+        santa.pop_front();
+    }
+    Ok(you.len() + santa.len())
+}
+
+/**
+ * Find the series of nodes leading to the target
+ */
+fn find_path(map: HashMap::<String, Vec<String>>, current: String, target: String) -> Result<VecDeque<String>, ()> {
+    if current == target {
+        let mut vec_deq = VecDeque::new();
+        vec_deq.push_front(current);
+        return Ok(vec_deq);
+    }
+    return map.get(&current).map_or(Err(()), |children| children.iter().fold(Err(()), |acc, child| {
+        match acc {
+            // We've already found the target
+            Ok(path) => Ok(path),
+            // Try to find the target
+            Err(_e) => {
+                match find_path(map.clone(), child.to_string(), target.clone()) {
+                    Ok(mut path) => {
+                        path.push_front(current.clone());
+                        Ok(path)
+                    },
+                    Err(_err) => Err(_err),
+                }
+            },
+        }
+    }))
 }
 
 fn count_orbits(map: HashMap::<String, Vec<String>>, node: String, depth: u32) -> u32 {
